de11e9549ca3baca31ae644ef40425e4e9b6771f
Jae Sung Kim <jae.sung.kim@cern.ch>
2018-01-22 09:06:35 +0900

    maketree update

--- maketree_BCDEF.sh
+++ maketree_BCDEF.sh
@@ -11,10 +11,10 @@ echo "####################"
 echo ""
 ./skimTree \
 /data1/Users/jskim/TagAndProbe/tree/TnPTree_Run2016BCDEF_dXYSIG_ADDED.root  \
-/data1/Users/jskim/TagAndProbe/tree/SkimmedForTrigger/TnPTree_Run2016BCDEF_dXYSIG_ADDED_Skimmed.root  \
+/data1/Users/jskim/TnP_Batch/tree/SkimmedForTrigger/TnPTree_Run2016BCDEF_dXYSIG_ADDED_Skimmed.root  \
 --remove "*" \
---keep "mass pt abseta dB dXYSig dzPV combRelIsoPF04dBeta tag_pt tag_combRelIsoPF04dBeta tag_IsoMu24 pt pair_probeMultiplicity DoubleIsoMu17Mu8_IsoMu17leg DoubleIsoMu17Mu8_IsoMu8leg DoubleIsoMu17TkMu8_IsoMu17leg DoubleIsoMu17TkMu8_IsoMu8leg tag_phi phi pair_deltaR Tight2012 tag_eta eta phi tag_phi Mu23_TrkIsoVVL IsoMu24 IsoTkMu24" \
---cut "tag_pt > 19.9999 && tag_combRelIsoPF04dBeta < 0.2 && tag_IsoMu24 == 1 && pt > 9.9999 && Tight2012 && dzPV > -0.5 && dzPV < 0.5 && combRelIsoPF04dBeta < 0.1 && dB < 0.05 && dXYSig < 3.0"
+--keep "mass pt abseta dB dXYSig dzPV combRelIsoPF04dBeta tag_pt tag_combRelIsoPF04dBeta tag_IsoMu24 pt pair_probeMultiplicity DoubleIsoMu17Mu8_IsoMu17leg DoubleIsoMu17Mu8_IsoMu8leg DoubleIsoMu17TkMu8_IsoMu17leg DoubleIsoMu17TkMu8_IsoMu8leg tag_phi phi pair_deltaR Tight2012 tag_eta eta phi tag_phi Mu23_TrkIsoVVL IsoMu24 IsoTkMu24 glbChi2" \
+--cut "tag_pt > 19.9999 && tag_combRelIsoPF04dBeta < 0.2 && tag_IsoMu24 == 1 && pt > 9.9999 && combRelIsoPF04dBeta < 0.2 && Tight2012 && dB < 0.01 && dzPV > -0.05 && dzPV < 0.05 && glbChi2 < 4 && dXYSig < 4.0"
 echo "Skimming done"
 
 echo ""
@@ -23,8 +23,8 @@ echo "Adding pair_dPhi"
 echo "####################"
 echo ""
 ./addBranch \
-/data1/Users/jskim/TagAndProbe/tree/SkimmedForTrigger/TnPTree_Run2016BCDEF_dXYSIG_ADDED_Skimmed.root \
-/data1/Users/jskim/TagAndProbe/tree/SkimmedForTrigger/TnPTree_Run2016BCDEF_dXYSIG_ADDED_Skimmed_pair_dPhi_ADDED.root \
+/data1/Users/jskim/TnP_Batch/tree/SkimmedForTrigger/TnPTree_Run2016BCDEF_dXYSIG_ADDED_Skimmed.root \
+/data1/Users/jskim/TnP_Batch/tree/SkimmedForTrigger/TnPTree_Run2016BCDEF_dXYSIG_ADDED_Skimmed_pair_dPhi_ADDED.root \
 "abs(tag_phi - phi) if abs(tag_phi - phi) < 3.1415926535 else 2*3.1415926535 - abs(tag_phi - phi);tag_eta;eta;phi;tag_phi" "pair_dPhi"
 
 echo "####################"
@@ -32,8 +32,8 @@ echo "Adding pair_dPhiPrimeDeg"
 echo "####################"
 echo ""
 ./addBranch \
-/data1/Users/jskim/TagAndProbe/tree/SkimmedForTrigger/TnPTree_Run2016BCDEF_dXYSIG_ADDED_Skimmed_pair_dPhi_ADDED.root \
-/data1/Users/jskim/TagAndProbe/tree/SkimmedForTrigger/TnPTree_Run2016BCDEF_dXYSIG_ADDED_Skimmed_pair_dPhi_ADDED_pair_dPhiPrimeDeg_ADDED.root \
+/data1/Users/jskim/TnP_Batch/tree/SkimmedForTrigger/TnPTree_Run2016BCDEF_dXYSIG_ADDED_Skimmed_pair_dPhi_ADDED.root \
+/data1/Users/jskim/TnP_Batch/tree/SkimmedForTrigger/TnPTree_Run2016BCDEF_dXYSIG_ADDED_Skimmed_pair_dPhi_ADDED_pair_dPhiPrimeDeg_ADDED.root \
 "pair_dPhi*(180/3.1415926535) if ((tag_eta > 0.9 and eta > 0.9) or (tag_eta < -0.9 and eta < -0.9)) else 999;pair_dPhi;tag_eta;eta;phi;tag_phi" "pair_dPhiPrimeDeg"
 echo ""
 echo "#### DONE ####"
