03fa1f4dbf1fe4afc877c47e5ccb50be0dc71a55
Jae Sung Kim <jae.sung.kim@cern.ch>
2018-01-23 12:23:25 +0900

    JH update

--- TagAndProbe_Trigger.sh
+++ TagAndProbe_Trigger.sh
@@ -10,8 +10,8 @@ echo "[bash] PassingProbeTrigger : "$PassingProbeTrigger
 echo "[bash] Period : "$Period
 echo "[bash] Syst : "$Syst
 
-filepath=/data1/Users/jskim/TagAndProbe/tree/SkimmedForTrigger
-resultpath=/data1/Users/jskim/TagAndProbe/results/$Syst/
+filepath=/data1/Users/jskim/TnP_Batch/tree/SkimmedForTrigger
+resultpath=/data1/Users/jskim/TnP_Batch/results/$Syst/
 mkdir -p $resultpath
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cd /data1/Users/jskim/cmssoftware/CMSSW_8_0_20/src
@@ -19,7 +19,7 @@ export SCRAM_ARCH=slc6_amd64_gcc530
 cmsenv
 cd /data1/Users/jskim/cmssoftware/CMSSW_8_0_20/src/MuonAnalysis/TagAndProbe/test/zmumu/
 echo "#### running fitter ####"
-cmsRun fitMuonTrigger_dilepton.py \
+cmsRun fitMuonTrigger_jh.py  \
 $filepath/$infile \
 $resultpath/$outfile \
 $PassingProbeTrigger \
--- submit_Trigger.py
+++ submit_Trigger.py
@@ -2,13 +2,13 @@ import os
 
 infile = ["MC.root", "BCDEF.root", "GH.root"]
 
-#triggers = ["DoubleIsoMu17Mu8_IsoMu17leg", "Mu8_OR_TkMu8"]
+triggers = ["DoubleIsoMu17Mu8_IsoMu17leg", "Mu8_OR_TkMu8"]
 #triggers = ["Mu23_TrkIsoVVL", 'IsoMu24_OR_IsoTkMu24']
-triggers = ['Mu8']
+#triggers = ['Mu8']
 
-systs = ["Central", "NMassBins30", "NMassBins50", "MassRange_60_130", "MassRange_70_120", "SignalShapeSingleV", "TagPt30Iso0p08", "TagPt20IsoInf", "ProbeMult99"]
-#systs = ["Central"]
-#systs = ["NMassBins30", "NMassBins50", "MassRange_60_130", "MassRange_70_120", "SignalShapeSingleV", "TagPt30Iso0p08", "TagPt20IsoInf", "ProbeMult99"]
+#systs = ["Central", "NMassBins30", "NMassBins50", "MassRange_60_130", "MassRange_70_120", "SignalShapeSingleV", "TagPt30Iso0p08", "TagPt20IsoInf", "ProbeMult99"]
+#systs = ["Central", "NMassBins30", "NMassBins50", "MassRange_60_130", "MassRange_70_120"]
+systs = ["SignalShapeSingleV", "TagPt30Iso0p08", "TagPt20IsoInf", "ProbeMult99"]
 
 #systs = ["Central"]
 #systs = ["NMassBins30", "NMassBins50"]
@@ -27,22 +27,22 @@ for syst in systs:
 
   for trigger in triggers:
     ## MC, BCDEF
-    cmd = 'qsub -q '+whichq+' -N MC_BtoF_'+trigger+'_'+syst+' -wd /data1/Users/jskim/TagAndProbe/logs/ TagAndProbe_Trigger.sh '+infile[0]+' '+trigger+'_MC_BCDEF.root '+trigger+' BCDEF '+syst
+    cmd = 'qsub -q '+whichq+' -N MC_BtoF_'+trigger+'_'+syst+' -wd /data1/Users/jskim/TnP_Batch/logs/ TagAndProbe_Trigger.sh '+infile[0]+' '+trigger+'_MC_BCDEF.root '+trigger+' BCDEF '+syst
     print cmd
     if DoSubmit:
       os.system(cmd)
     ## MC, GH
-    cmd = 'qsub -q '+whichq+' -N MC_GtoH_'+trigger+'_'+syst+' -wd /data1/Users/jskim/TagAndProbe/logs/ TagAndProbe_Trigger.sh '+infile[0]+' '+trigger+'_MC_GH.root '+trigger+' GH '+syst
+    cmd = 'qsub -q '+whichq+' -N MC_GtoH_'+trigger+'_'+syst+' -wd /data1/Users/jskim/TnP_Batch/logs/ TagAndProbe_Trigger.sh '+infile[0]+' '+trigger+'_MC_GH.root '+trigger+' GH '+syst
     print cmd
     if DoSubmit:
       os.system(cmd)
     ## Data, BCDEF
-    cmd = 'qsub -q '+whichq+' -N Data_BtoF_'+trigger+'_'+syst+' -wd /data1/Users/jskim/TagAndProbe/logs/ TagAndProbe_Trigger.sh '+infile[1]+' '+trigger+'_Data_BCDEF.root '+trigger+' BCDEF '+syst
+    cmd = 'qsub -q '+whichq+' -N Data_BtoF_'+trigger+'_'+syst+' -wd /data1/Users/jskim/TnP_Batch/logs/ TagAndProbe_Trigger.sh '+infile[1]+' '+trigger+'_Data_BCDEF.root '+trigger+' BCDEF '+syst
     print cmd
     if DoSubmit:
       os.system(cmd)
     ## Data, GH
-    cmd = 'qsub -q '+whichq+' -N Data_GtoH_'+trigger+'_'+syst+' -wd /data1/Users/jskim/TagAndProbe/logs/ TagAndProbe_Trigger.sh '+infile[2]+' '+trigger+'_Data_GH.root '+trigger+' GH '+syst
+    cmd = 'qsub -q '+whichq+' -N Data_GtoH_'+trigger+'_'+syst+' -wd /data1/Users/jskim/TnP_Batch/logs/ TagAndProbe_Trigger.sh '+infile[2]+' '+trigger+'_Data_GH.root '+trigger+' GH '+syst
     print cmd
     if DoSubmit:
       os.system(cmd)
